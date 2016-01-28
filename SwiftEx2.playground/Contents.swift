09ba826e422350dc50645d765dea3df6cf121d47
Peter Carswell <pcarswel@cscc.edu>
2016-01-27 20:58:34 -0500

    lab 2 first attempt

---
+++ SwiftEx2.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+/*:
+**Exercise:** Get Acquainted. Get a few properties of each person in your class... name and email are good. Use the `String` and `Character` as well as the data types and operators in [Basics | Data Types | Operators](Basics%20|%20Data%20Types%20|%20Operators) to complete this exercise.
+>> **Example Output:**
+* `My name is Mathew Sheets, my email is swift.dude@gmail.com`
+* `Other students in my class are Annie, Sam, Jack, Hudson, Oliver`
+* `Oliver dropped this class` (**use the string above to pull out the last persons name**)
+* `CARSON was added to the class` (**use the string above and insert another person before the second person**)
+* `Jack and Hudson are in the class` (**use the string above to pull out fourth and fifth person**)
+*/
+
